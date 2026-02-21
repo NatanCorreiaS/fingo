359dc328841ad3929e29e79a06270ad87536f941
natan <natancorreiatr@gmail.com>
2026-02-21 20:32:23 -0300

    fix: change money type to int64

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"log"
+	"natan/fingo/dbsqlite"
 	"net/http"
 )
 
 func main() {
+	if err := dbsqlite.CheckAndCreate(); err != nil{
+		log.Println(err)
+	}
 	log.Printf("Server listening on PORT 8000...")
 	mux := RouterMux()
 	err := http.ListenAndServe(":8000", mux)
--- utils/money.go
+++ utils/money.go
@@ -1,13 +1,12 @@
 package utils
 
 import (
-	"errors"
 	"math"
 )
 
 // Money represents a monetary value stored in cents to avoid floating-point precision issues.
-// For example, R$ 10.50 is stored as 1050.
-type Money uint64
+// For example, R$ 10.50 is stored as 1050. Negative values represent a deficit balance.
+type Money int64
 
 // ConvertToFloat converts the Money value from cents to a float64 representation.
 func (m *Money) ConvertToFloat() float64 {
@@ -15,11 +14,7 @@ func (m *Money) ConvertToFloat() float64 {
 }
 
 // ConvertToInt converts a float64 value to cents and stores it in the Money receiver.
-// Returns an error if the value is negative.
 func (m *Money) ConvertToInt(value float64) error {
-	if value < 0 {
-		return errors.New("value must be greater than 0")
-	}
 	*m = Money(math.Round(value * 100))
 	return nil
 }
